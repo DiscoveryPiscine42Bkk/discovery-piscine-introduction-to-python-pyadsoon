2e6ed4a99a2157e4d914b63e621298e3989804a3
pyadsoon <phatdanai013@gmail.com>
2025-06-16 14:45:00 +0700

    Create iszero.py

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,6 @@
+number = float(input("please enter a number: "
+
+                     if number == 0:
+                     prist("This number is equal to zero.")
+                     else:
+               print("this number is different from zero.
